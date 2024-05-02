a8746c3f084bcd0b7b3e8c458b7e41b81124a0b3
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-05-02 15:34:14 +0200

    add form fields

--- guestbook/resources/views/index.blade.php
+++ guestbook/resources/views/index.blade.php
@@ -5,6 +5,24 @@
 <div class="container">
     <h1>Gästebuch</h1>
     <form method="GET" action="{{route('index', [], false)}}">
+        <div class="mb-3">
+            <label for="inputUsername" class="form-label">Username</label>
+            <input type="text" class="form-control" id="inputUsername">
+        </div>
+        <div class="mb-3">
+            <label for="inputEmail" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail">
+        </div>
+        <div class="mb-3">
+            <label for="inputSubtitle" class="form-label">Subtitle</label>
+            <input type="text" class="form-control" id="inputSubtitle">
+        </div>
+        <div class="mb-3">
+            <label for="inputBody" class="form-label">Body</label>
+            <textarea id="inputBody" class="form-control" id="inputBody"></textarea>
+        </div>
+        <div class="mb-3">
+            <button class="btn btn-primary">Speichern</button>
     </form>
 </div>
 
