5628950878fefe90ee6275d3a1f5191c352ec532
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-05-02 15:38:21 +0200

    enable POST-Method

--- guestbook/routes/web.php
+++ guestbook/routes/web.php
@@ -3,4 +3,4 @@
 use App\Controller\IndexController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [IndexController::class, 'indexAction'])->name('index');
+Route::match(['GET', 'POST'], '/', [IndexController::class, 'indexAction'])->name('index');
