5ae663e1464c45c73565d9c9ac5b8966a6c4c505
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-06-14 15:43:42 +0200

    add pagination

--- guestbook/app/Controller/IndexController.php
+++ guestbook/app/Controller/IndexController.php
@@ -20,10 +20,20 @@ class IndexController{
             GuestBookEntry::create($validated);
             return redirect()->route('index')->with('success', 'Erfolgreich gespeichert!');
         }
+        $limit = 1;
+        $maxEntries = GuestBookEntry::count();
+        $maxPages = (int)ceil($maxEntries/$limit);
+        $page = (int)$request->get('page', 1);
+        $page = min(max(1, $page), $maxPages);
+        $offset = ($page -1) * $limit;
 
-        $entries = GuestBookEntry::query()->orderBy('created_at', 'DESC')->get();
+        $entries = GuestBookEntry::query()
+        ->orderBy('created_at', 'DESC')
+        ->offset($offset)
+        ->limit($limit)
+        ->get();
         
-        return view('index', ['entries'=>$entries]);
+        return view('index', ['entries'=>$entries, 'maxPages'=>$maxPages, 'currentPage'=>$page]);
 
     }
 
