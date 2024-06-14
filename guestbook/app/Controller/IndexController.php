2fb69924b4844fa7956ec772850e9729a8af5581
Nirvous19 <90237821+Nirvous19@users.noreply.github.com>
2024-06-14 14:35:59 +0200

    validating

--- guestbook/app/Controller/IndexController.php
+++ guestbook/app/Controller/IndexController.php
@@ -2,11 +2,23 @@
 
 namespace App\Controller;
 
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
 class IndexController{
 
-    public function indexAction()
+    public function indexAction(Request $request): View
     {
-        
+        if($request->getMethod() == 'POST'){
+            $validated = $request->validate([
+                'username' => 'required|max:255',
+                'email' => 'email|max:255|nullable',
+                'subtitle' =>'required|max:255',
+                'body' => 'required'
+            ]);
+            dump($validated);
+        }
+
         return view('index');
 
     }
--- guestbook/resources/views/index.blade.php
+++ guestbook/resources/views/index.blade.php
@@ -4,23 +4,33 @@
 @section('content')
 <div class="container">
     <h1>Gästebuch</h1>
-    <form method="GET" action="{{route('index', [], false)}}">
+    @if($errors->any()) 
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error) 
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif 
+
+    <form method="POST" action="{{route('index', [], false)}}">
         @csrf
         <div class="mb-3">
             <label for="inputUsername" class="form-label">Username</label>
-            <input type="text" class="form-control" id="inputUsername">
+            <input type="text" value="{{old('username')}}" class="form-control @error('username') is-invalid @enderror" name="username" id="inputUsername">
         </div>
         <div class="mb-3">
             <label for="inputEmail" class="form-label">Email</label>
-            <input type="email" class="form-control" id="inputEmail">
+            <input type="email" value="{{old('email')}}" class="form-control @error('email') is-invalid @enderror" name="email" id="inputEmail">
         </div>
         <div class="mb-3">
             <label for="inputSubtitle" class="form-label">Subtitle</label>
-            <input type="text" class="form-control" id="inputSubtitle">
+            <input type="text" value="{{old('subtitle')}}" class="form-control @error('subtitle') is-invalid @enderror" name="subtitle" id="inputSubtitle">
         </div>
         <div class="mb-3">
             <label for="inputBody" class="form-label">Body</label>
-            <textarea id="inputBody" class="form-control" id="inputBody"></textarea>
+            <textarea id="inputBody" class="form-control @error('body') is-invalid @enderror" name="body">{{old('body')}}</textarea>
         </div>
         <div class="mb-3">
             <button class="btn btn-primary">Speichern</button>
